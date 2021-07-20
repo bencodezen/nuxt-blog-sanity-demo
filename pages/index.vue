496c9154b79c8337bc4b7bf6418b45b2f74a477e
Ben Hong <ben@bencodezen.io>
2021-07-20 13:34:29 -0400

    feature: add integration with sanity

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Hello Twitch!</h1>
+    <ul>
+      <li v-for="post in posts" :key="post._id">
+        {{ post.title }}
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import { groq } from '@nuxtjs/sanity'
+
+const query = groq`{ "posts": *[_type == "post"] }`
+
+export default {
+  asyncData({ $sanity }) {
+    return $sanity.fetch(query)
+  },
+}
 </script>
